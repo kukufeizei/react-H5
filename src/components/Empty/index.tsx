6e376a6ab042744025f2a342dbcda053acc13883
LAPTOP-72GIOSV0\ly <422048790@qq.com>
2022-09-26 17:44:33 +0800

    update:empty 新增泛型

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -1,8 +1,8 @@
-import { memo } from 'react';
+import { memo, FC } from 'react';
 import { ErrorBlock } from 'antd-mobile'
 import './index.less'
 import type { EmptyTypes } from './type'
-const EmptyItem = (props: EmptyTypes) => {
+const EmptyItem: FC<EmptyTypes> = (props) => {
     return (
         <div className='item_content empty'>
             <ErrorBlock status={props.status} />
